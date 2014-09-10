6f89bf05cd3bf7f196d07987cace25f63c8a46c1
Gary Rennie <webmaster@gazler.com>
2014-09-10 12:23:39 +0100

    feat(Gruntfile.js): use connect and watch to livereload index page
    A base index.html page has been created and served using Grunt.
    
    The following Grunt plugins are used:
    
     * https://github.com/gruntjs/grunt-contrib-connect
     * https://github.com/gruntjs/grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,2 +1,24 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+    watch: {
+      files: ['index.html'],
+      options: {
+        livereload: true,
+         nospawn: true
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 3001,
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['connect', 'watch']);
 }
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
  1) Run `npm install` to install the dependencies.
  2) Run `bower install` to fetch the bower dependencies
+ 3) Start the server using `grunt`
