2d708ccb6297059127b55763bcb643cef938d76a
Gary Rennie <webmaster@gazler.com>
2014-09-11 09:15:54 +0100

    feat(Gruntfile.js): allow less compiling for bootstrap vars
    The Gruntfile now includes a `less` task for compiling less assets. This
    task is automatically run when a change is made to `index.html` or
    `variables.less`
    
    It compiles the `build.less` file which compiles the stock bootstrap
    file with the variable overrides.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,13 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
   grunt.initConfig({
+    builddir: 'build',
     watch: {
-      files: ['index.html'],
+      files: ['index.html', 'variables.less'],
+      tasks: ['less'],
       options: {
         livereload: true,
          nospawn: true
@@ -15,6 +18,13 @@ module.exports = function(grunt) {
         options: {
           port: 3001,
           livereload: true
+        }
+      }
+    },
+    less: {
+      development: {
+        files: {
+          "build/bootstrap.css": "build.less"
         }
       }
     }
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
  1) Run `npm install` to install the dependencies.
  2) Run `bower install` to fetch the bower dependencies
  3) Start the server using `grunt`
+ 4) Make changes and the page will live reload
