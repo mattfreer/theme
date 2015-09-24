5e1641c3d841fad4b6bc6b77865b8541f124fa42
Harvey Ball <me@harveyball.com>
2015-09-24 13:09:11 +0100

    feat(site-builder): add grunt task to auto build public content
    A new grunt task has been added that will auto build the new multi page
    theme public website based off of html templates and html source files.
    
    The html templates and the source files reside in the new `docs/`
    directory along with a `build.json` file which acts as the build
    manifest to determine which pages get built and in which order.
    
    The main heading of the mainfest determines the html file, the following
    heading determines the main title section and the subsequent headings
    determine headings for the elements.
    
    i.e.
    
    ```
      "styles": {
        "typography": [
          "headings"
        ]
      }
    
      styles (will generate styles.html)
      and generate a H1 title of "typography"
      with a generated H2 section for "headings"
    ```
    
    The build manifest file directly relates to a directory structure within
    the `docs/` directory. So `styles:typography:headings` is mapped to the
    following file:
    
    `docs/styles/typography/headings.html`
    
    The source html files are an enhanced markup allowing for easier styling
    of elements without needing to know the relevant classes. Any html is
    allowed with the addition of these two tags:
    
      * `<title>My Title</title>`
        * Will generate a H3 tag appropriately styled.
      * `<example>my example code</example>`
        * Will generate an example container with a 'view source' button.
    
    the `public/` directory will not be auto generated so it cannot be
    deleted and is required to be checked into the repository along with any
    files that are contained within it.

--- public/js/page.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  $('.ls-component [data-toggle="popover"]').popover();
-  $('.ls-component [data-toggle="tooltip"]').tooltip();
-})();
---
+++ tasks/build-docs.js
@@ -0,0 +1,123 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+var sourceDir = __dirname + "/../docs/";
+
+function fileExists(filePath) {
+  try {
+    return fs.statSync(filePath).isFile();
+  }
+  catch (err) {
+    return false;
+  }
+}
+
+function readFile(filePath, requiredExist) {
+  var source = sourceDir + filePath;
+  if (fileExists(source)) {
+    return fs.readFileSync(source, "utf8");
+  }
+  if(requiredExist == true) {
+    throw new Error("File required but not found: " + source);
+  }
+  return null;
+}
+
+function cleanTitle(title) {
+  return title.replace(/-/g, " ");
+}
+
+module.exports = function(grunt) {
+  grunt.registerTask("buildDocs", "Build the theme documentation", function() {
+    var version = grunt.config.get("version");
+
+    if (!version) {
+      grunt.log.error("No version config set");
+      return false;
+    }
+
+    var outputDir = __dirname + "/../public/";
+
+    var buildScript = JSON.parse(readFile("build.json"), true);
+
+    var headerTemplate = readFile("core/header.html", true);
+    var footerTemplate = readFile("core/footer.html", true);
+    var pageTitleTemplate = readFile("core/banner.html");
+
+    var docsSectionHeaderTemplate = readFile("core/docs-section-header.html");
+    var docsSectionFooterTemplate = readFile("core/docs-section-footer.html");
+    var docsSectionBodyTemplate = readFile("core/docs-section-body.html");
+    var docsSectionBlockHeaderTemplate = readFile("core/docs-section-block-header.html");
+    var docsSectionBlockFooterTemplate = readFile("core/docs-section-block-footer.html");
+    var docsSectionBlockTitleTemplate = readFile("core/docs-section-block-title.html");
+    var docsSectionBlockExampleTemplate = readFile("core/docs-section-block-example.html");
+
+    for(var pageName in buildScript) {
+      var pageFileName, mainMenuContent, pageContent, pageOutput;
+
+      pageFileName = pageName + ".html";
+      mainMenuContent = "";
+
+      //build main menu
+      for(var menuPageName in buildScript) {
+        var menuPageFileName, menuItemClass;
+
+        menuPageFileName = menuPageName + ".html";
+        menuItemClass = "text-capitalize";
+        if (pageFileName == menuPageFileName) {
+          menuItemClass += " active";
+        }
+        mainMenuContent += '<li class="' + menuItemClass + '"><a href="' + menuPageFileName + '">' + menuPageName + '</a></li>'
+      }
+
+      pageContent = buildScript[pageName];
+      //set active menu item
+      pageOutput = headerTemplate
+                   .replace(/\$MainMenu\$/g, mainMenuContent);
+
+      //generate page banner content
+      var pageTitleData = readFile(pageName + "/banner.html")
+                          .replace(/<title>/g, "<h1 class='text-capitalize'>")
+                          .replace(/<\/title>/g, "</h1>");
+      pageOutput += pageTitleTemplate
+                    .replace(/\$Body\$/g, pageTitleData);
+
+      //loop through each content section for page
+      for(var sectionName in pageContent) {
+        var blockContents = pageContent[sectionName];
+        var pageBlocksPath = pageName + "/" + sectionName + "/";
+
+        //Build section h1 and optional introduction body content
+        pageOutput += docsSectionHeaderTemplate.replace(/\$Title\$/g, cleanTitle(sectionName));
+
+        if (fileExists(sourceDir + pageBlocksPath + "index.html")) {
+          pageOutput += docsSectionBodyTemplate
+                        .replace(/\$Body\$/g, readFile(pageBlocksPath + "index.html"));
+        }
+
+        for(var i = 0; i < blockContents.length; i++) {
+          var pageBlockName = blockContents[i];
+          var pageBlockDataBody = readFile(pageBlocksPath + pageBlockName + ".html")
+                                  .replace(/<title>/g, "<h3 class='page-header text-capitalize'>")
+                                  .replace(/<\/title>/g, "</h3>")
+                                  .replace(/<example>/g, "<div class='ls-component'>")
+                                  .replace(/<\/example>/g, "</div>");
+
+          pageOutput += docsSectionBlockHeaderTemplate
+                        .replace(/\$FullTitle\$/g, sectionName + "-" + pageBlockName)
+                        .replace(/\$Title\$/g, cleanTitle(pageBlockName))
+                        .replace(/\$Body\$/g, pageBlockDataBody);
+
+          pageOutput += docsSectionBlockFooterTemplate
+                        .replace(/\$FullTitle\$/g, sectionName + "-" + pageBlockName)
+        }
+
+        pageOutput += docsSectionFooterTemplate.replace(/\$Title\$/g, sectionName);
+      }
+
+      pageOutput += footerTemplate;
+
+      fs.writeFile(outputDir + pageFileName, pageOutput, function(err) {});
+    }
+  });
+};
